c603a86e20849e0a30c95c6e9ad21ab272e8b9ce
edwoodward <ecw1@rice.edu>
2020-06-16 21:01:06 -0500

    Cleaned up code and added student edit

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,5 @@
 class StudentsController < ApplicationController
+  before_action :set_student, only: [:show, :edit, :update]
   def index 
     @students = Student.all
   end 
@@ -17,7 +18,27 @@ class StudentsController < ApplicationController
     end
   end
 
+  def show 
+  end 
+
+  def edit 
+  end 
+
+  def update 
+    if @student.update(student_params) 
+      flash[:success] = "You have successfully updated your profile"
+      redirect_to student_path(@student)
+    else 
+      render 'edit'
+    end
+
+  end 
+
   private 
+
+  def set_student
+    @student = Student.find(params[:id])
+  end
 
   def student_params
     params.require(:student).permit(:name, :email)
