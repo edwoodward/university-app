d7e09f534a104f3f8a0a2407bffc3e843f3529f2
edwoodward <ecw1@rice.edu>
2020-06-17 20:53:21 -0500

    Added password field to student and edit fields

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,4 +3,5 @@ class Student < ApplicationRecord
   validates :name, presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: {case_sensiticwe: false}, length: {maximum:105}, format: {with: VALID_EMAIL_REGEX}
+  has_secure_password
 end 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_12_182653) do
+ActiveRecord::Schema.define(version: 2020_06_18_004738) do
 
   create_table "courses", force: :cascade do |t|
     t.string "short_name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_06_12_182653) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
